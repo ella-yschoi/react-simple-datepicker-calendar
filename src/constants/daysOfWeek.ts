d0959cf00799f3c4464daf4e69d3fd1e1287b87f
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-07 22:24:31 +0900

    feat: Add Constants for 'dateFormat' and 'daysOfWeek'

---
+++ src/constants/daysOfWeek.ts
@@ -0,0 +1,21 @@
+const DAYS_OF_WEEK_EN = [
+  'SUN',
+  'MON',
+  'TUE',
+  'WED',
+  'THU',
+  'FRI',
+  'SAT',
+];
+
+const DAYS_OF_WEEK_KO = [
+  '일',
+  '월',
+  '화',
+  '수',
+  '목',
+  '금',
+  '토',
+];
+
+export { DAYS_OF_WEEK_EN, DAYS_OF_WEEK_KO  }
