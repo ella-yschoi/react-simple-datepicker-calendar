e42fa568a5a5cb7917c25fe0236f0e5644642311
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-13 16:53:40 +0900

    build: Updated package.json, tsconfig.json and vite.config.ts for Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      name: 'react-simple-datepicker',
+      entry: 'src/index.ts',
+      formats: ["es", "umd", "cjs"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
+
