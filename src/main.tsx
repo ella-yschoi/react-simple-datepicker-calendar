7ceedb0358de1a02c2cadd310e9dbcdbdb87a1f3
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-14 17:12:39 +0900

    fix: Pass required CalendarProps to App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const calendarProps = {
+  calendarBackgroundColor:'#252525',
+  displayBackgroundColor: '#252525',
+  displayFontColor: '#c5c5c5',
+  dayFontColor: '#899797',
+  currentDateFontColor: '#d5d5d5',
+  prevNextDateFontColor: '#899797',
+};
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App {...calendarProps} />
   </React.StrictMode>
 );
